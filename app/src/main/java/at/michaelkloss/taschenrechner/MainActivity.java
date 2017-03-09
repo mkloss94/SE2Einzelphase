b34f463c2791e12c0be78ba24b22f4c08b6c71e9
mkloss94 <m1kloss@edu.aau.at>
2017-03-09 22:44:15 +0100

    Nun wurde die Funktion onClick erweitert, damit es Benutzern nicht mehr möglich ist, die Felder leer zu lassen
    Edit: Zusätzlich wurde eine Bedingung erstellt, die das durch 0 dividieren verhindert und eine Fehlernachricht ausgibt.
    
    Was noch hinzukommt, sind die Kommentare in dem Source-Code (Dienen der Erklärung der einzeilnen Codestellen).

--- app/src/main/java/at/michaelkloss/taschenrechner/MainActivity.java
+++ app/src/main/java/at/michaelkloss/taschenrechner/MainActivity.java
@@ -1,5 +1,6 @@
 package at.michaelkloss.taschenrechner;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //Die Initialisierungen
     EditText num1;
     EditText num2;
     TextView result;
@@ -19,22 +21,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Der Button wird "geholt" und anschließend ein onClickListener aufgerufen. (Deswegen das Interface View. OnClickListener)
         btnDividieren = (Button) findViewById(R.id.button);
         btnDividieren.setOnClickListener(this);
 
     }
 
+    //diese Methode wird durch das Interface bereitgestellt.
     @Override
     public void onClick(View v) {
 
+        //Nun wird der Dividend und der Divisor sowie das Ergebnis geholt.
         num1 = (EditText) findViewById(R.id.numberfieldDividend);
         num2 = (EditText) findViewById(R.id.numberfieldDivisor);
         result = (TextView) findViewById(R.id.result);
-        int newResult = 0;
+        int newResult = 0; //wird sicherheitshalber mit 0 initialisiert.
 
+        //Nun werden die Werte der beiden EditText-Felder in die jeweiligen Variablen zugewiesen. (ACHTUNG als String)
         String first = num1.getText().toString();
         String second = num2.getText().toString();
 
+        //Diese Überprüfungen dienen der Sicherheit, damit der User kein Feld leer lässt.
         if(TextUtils.isEmpty(first)){
             num1.setError("Das Dividenden-Feld darf nicht leer sein!");
             return;
@@ -45,14 +52,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return;
         }
 
-        int dividend = Integer.parseInt(num1.getText().toString());
-        int divisor = Integer.parseInt(num2.getText().toString());
+        //jetzt werden die Strings in Integer umgewandelt und wieder in neue Variablen gespeichert.
+        int dividend = Integer.parseInt(first);
+        int divisor = Integer.parseInt(second);
 
-
-
-        newResult = dividend / divisor;
-
-        String txt = "Das Ergebnis aus " + dividend + "/" + divisor + " lautet: ";
-        result.setText(txt + newResult);
+        /*Überprüfung, ob der Divisor gleich 0 ist....um Fehlerquellen zu reduzieren. Es wird anschließend
+        * das TextView Element rot gefärbt und anschließend eine Fehlernachricht ausgegegeben.*/
+        if(divisor == 0){
+            result.setTextColor(Color.RED);
+            result.setText("Die Division " + dividend +"/0 ist nicht möglich");
+            return;
+        } else {
+            //hier wird die Farbe wieder gewechselt
+            result.setTextColor(Color.BLACK);
+            /*anschließend die wirkliche Berechnung, die in einem TRY-CATCH Block eingehüllt wird um
+            eventuelle Fehlerquellen abzufangen.*/
+            try {
+                newResult = dividend / divisor;
+                //String für die Ausgabe
+                String txt = "Das Ergebnis aus " + dividend + ":" + divisor + " lautet: ";
+                //Das Result-TextView-Element wird wieder neu beschrieben
+                result.setText(txt + newResult);
+            } catch (Exception e){
+                System.out.println("Error! Es hat etwas nicht geklappt: " + e.getMessage());
+            }
+        }
     }
 }
